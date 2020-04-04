3f24753a47c0232b18e9c6861894361d281555d1
kimani <kimani@diotech>
2020-04-04 19:56:06 +0300

    added dockerization and changed network to host

--- replicator/setup.sh
+++ replicator/setup.sh
@@ -5,8 +5,8 @@ if [ -f /replicated.txt ]; then
 else
   echo "Setting up mongo replication ..."
   # Wait for few seconds until the mongo server is up
-  sleep 10
-  mongo db:27017 replicate.js
+  sleep 30
+  mongo localhost:27017 replicate.js
   echo "Replication done..."
   # Wait for few seconds until replication takes effect
   # sleep 20
